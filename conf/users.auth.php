55b5f92999ed233661526637940f60093f6da31a
pengfei <liu.pengfei@hotmail.fr>
2020-09-23 10:33:49 +0200

    all of it

---
+++ conf/users.auth.php
@@ -0,0 +1,11 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+pliu:$1$LTKwKmoX$dgBrinCYVvV7RuCfo8vV9/:pengfei liu:liu.pengfei@hotmail.fr:admin,user
+visitor:$1$b3LX57QF$q9xd6xWU5Qqsh/M.An20g1:visitor:liu.pengfei@hotmail.fr:visitor,user
